3d6d2f33f9549bf1f36d60f73314ddcfb8d3de71
Debanjan <debanjanacharya21@gmail.com>
2021-09-15 13:41:32 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [weather, setWeather] = useState(null)
+  const [input, setInput] = useState("")
+  useEffect(() => {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=Delhi&aqi=no`)
+      .then(data => {
+        setWeather(data.data);
+      })
+      .catch(err => console.log(err));
+  }, [])
+
+  // Event
+  const weatherInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const searchWeather = () => {
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=e4d324a6ff3445229f554012211509&q=${input}&aqi=no`)
+      .then(data => {
+        setWeather(data.data);
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {weather && (
+        <div className="app">
+          <div>
+            <input onChange={weatherInput} type="text" />
+            <button onClick={searchWeather}>Search</button>
+          </div>
+          <h1>{weather.location.country}</h1>
+          <h1>{weather.location.name}</h1>
+          <h2>{weather.location.region}</h2>
+          <div className="condition">
+            <h3>{weather.current.condition.text}</h3>
+            <img src={weather.current.condition.icon} alt="condition" />
+            <h3>{weather.current.temp_c} ° Celsius</h3>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
